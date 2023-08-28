6233955fb4b25538975279f0af2e796fc9071d11
desokan <thefdmm@gmail.com>
2023-08-28 11:36:18 +0100

    created the topic controller

---
+++ controllers/topicController.js
@@ -0,0 +1,18 @@
+import Topic from "../models/topicModel";
+
+export const createTopic = async (req, res) => {
+  try {
+    const newTopic = await Topic.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        topic: newTopic,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
