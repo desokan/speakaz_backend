3a41874ece2be41e0a8626c8e87afeda07fdb57a
desokan <thefdmm@gmail.com>
2023-08-28 11:42:45 +0100

    created the create topic route

---
+++ routes/topicRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import * as topicController from '../controllers/topicController.js'
+
+const router = express.Router()
+
+router.route('/').post(topicController.createTopic)
+
+export default router
