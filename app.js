fa46d31ac6a2be1c41287d7cad407e4ed5bd7838
desokan <thefdmm@gmail.com>
2023-08-28 11:04:43 +0100

    added middleware that creates the time a request was made to the database

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 import express from "express";
 import morgan from "morgan";
-import { getPublicPath } from "./utils/pathUtils.js";
+import { requestTime } from "./middleware/reqMiddleware.js";
 
 const app = express();
 
 // MIDDLEWARES
-app.use(express.static(getPublicPath()));
 app.use(express.json());
+app.use(requestTime);
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// ROUTES
 
 export default app;
